132dc1060d9f33d3f2c9853ed1530751664d81ba
zwb <1210611257@qq.com>
2024-11-12 16:17:27 +0800

    Add config generator

--- tools/gguf_config_to_json/main.cpp
+++ tools/gguf_config_to_json/main.cpp
@@ -24,7 +24,7 @@ static float get_f32(gguf_context *ctx, const std::string &key, bool required = 
 }
 
 static std::string get_str(
-    gguf_context *ctx, const std::string &key, bool required = true, std::string default_value = ""
+    gguf_context *ctx, const std::string &key, bool required = true, const std::string &default_value = ""
 ) {
     int idx = gguf_find_key(ctx, key.c_str());
     if (idx == -1) {
@@ -39,7 +39,7 @@ void collect_config(gguf_context *gguf_ctx, nlohmann::json &config);
 int main(int argc, char *argv[]) {
     std::string file_path   = "/home/zwb/Downloads/Llama-2-7b-chat-hf/llama-2-7b.f32.gguf";
     std::string target_path = "./llama-2-7b.json";
-    CLI::App app("GGUF Converter");
+    CLI::App app("Config Generator");
 
     app.add_option("--file-path", file_path)->required();
     app.add_option("--target-path", target_path)->required();
@@ -67,17 +67,18 @@ int main(int argc, char *argv[]) {
 void collect_config(gguf_context *ctx, nlohmann::json &config) {
     std::string model_arch = gguf_get_val_str(ctx, gguf_find_key(ctx, "general.architecture"));
     config["model_arch"]   = model_arch;
+    auto get_arch_config([&model_arch](const std::string c) { return fmt::format(fmt::runtime(c), model_arch); });
+
     { // embd_dim, ffn_dim, n_heads, n_kv_heads, n_layers, n_ctx
-        // TODO: name?
-        config["embd_dim"]   = get_u32(ctx, fmt::format("{}.embedding_length", model_arch));
-        config["ffn_dim"]    = get_u32(ctx, fmt::format("{}.feed_forward_length", model_arch));
-        config["n_heads"]    = get_u32(ctx, fmt::format("{}.attention.head_count", model_arch));
-        config["n_kv_heads"] = get_u32(ctx, fmt::format("{}.attention.head_count_kv", model_arch));
-        config["n_layers"]   = get_u32(ctx, fmt::format("{}.block_count", model_arch));
-        config["n_ctx"]      = get_u32(ctx, fmt::format("{}.context_length", model_arch));
+        config["embd_dim"]        = get_u32(ctx, get_arch_config("{}.embedding_length"));
+        config["ffn_dim"]         = get_u32(ctx, get_arch_config("{}.feed_forward_length"));
+        config["n_attn_heads"]    = get_u32(ctx, get_arch_config("{}.attention.head_count"));
+        config["n_attn_kv_heads"] = get_u32(ctx, get_arch_config("{}.attention.head_count_kv"));
+        config["n_layers"]        = get_u32(ctx, get_arch_config("{}.block_count"));
+        config["n_ctx"]           = get_u32(ctx, get_arch_config("{}.context_length"));
     }
     { // vocab_size
-        auto idx = gguf_find_key(ctx, fmt::format("{}.vocab_size", model_arch).c_str());
+        auto idx = gguf_find_key(ctx, get_arch_config("{}.vocab_size").c_str());
         if (idx != -1) {
             config["vocab_size"] = gguf_get_val_u32(ctx, idx);
         } else {
@@ -85,5 +86,55 @@ void collect_config(gguf_context *ctx, nlohmann::json &config) {
             SMART_ASSERT(idx != -1);
             config["vocab_size"] = gguf_get_arr_n(ctx, idx);
         }
+    }
+    { // kv_dim, norm_eps or norm_rms_eps
+        if (config["n_attn_heads"] > 0) {
+            auto default_kv_dim = (uint32_t)config["embd_dim"] / (uint32_t)config["n_attn_heads"];
+            auto k_dim          = get_u32(ctx, get_arch_config("{}.attention.key_length"), false, default_kv_dim);
+            auto v_dim          = get_u32(ctx, get_arch_config("{}.attention.value_length"), false, default_kv_dim);
+            SMART_ASSERT(k_dim == v_dim);
+            config["kv_dim"] = k_dim;
+        } else {
+            config["kv_dim"] = 0;
+        }
+
+        auto idx      = gguf_find_key(ctx, get_arch_config("{}.attention.layer_norm_epsilon").c_str());
+        auto norm_eps = 1e-5f;
+        if (idx == -1) {
+            idx = gguf_find_key(ctx, get_arch_config("{}.attention.layer_norm_rms_epsilon").c_str());
+        }
+        if (idx != -1) {
+            norm_eps = gguf_get_val_f32(ctx, idx);
+        }
+        config["norm_eps"] = std::to_string(norm_eps);
+        // Note: float -> string for percision
+        fmt::println(
+            "norm_eps: {} -> {} -> {}",
+            norm_eps,
+            (std::string)config["norm_eps"],
+            std::stof((std::string)config["norm_eps"])
+        );
+    }
+    { // rope_dim
+        config["rope_dim"] =
+            get_u32(ctx, get_arch_config("{}.rope.dimension_count"), false, (uint32_t)config["kv_dim"]);
+        config["rope_freq_base"]  = std::to_string(get_f32(ctx, get_arch_config("{}.rope.freq_base"), false, 10000.0f));
+        auto scale_type           = get_str(ctx, get_arch_config("{}.rope.scaling.type"), false, "linear");
+        config["rope_scale_type"] = scale_type;
+        config["rope_attn_factor"] =
+            std::to_string(get_f32(ctx, get_arch_config("{}.rope.scaling.attn_factor"), false, 1.0f));
+        config["n_rope_ctx_orig"] =
+            get_u32(ctx, get_arch_config("{}.rope.scaling.original_context_length"), false, (uint32_t)config["n_ctx"]);
+
+        auto rope_scale = 0.0f;
+        auto idx        = gguf_find_key(ctx, get_arch_config("{}.rope.scaling.factor").c_str());
+        if (idx == -1) {
+            idx = gguf_find_key(ctx, get_arch_config("{}.rope.scale_linear").c_str());
+        }
+        if (idx != -1) {
+            rope_scale = gguf_get_val_f32(ctx, idx);
+        }
+        rope_scale                = rope_scale == 0.0f ? 1.0f : 1.0f / rope_scale;
+        config["rope_freq_scale"] = std::to_string(rope_scale);
     }
 }
